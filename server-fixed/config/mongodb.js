6abe527412564b1f3641b44b73f3c5d26f825bed
Akshatsingh200 <singhakshatsingh83@gmail.com>
2026-04-26 23:03:31 +0530

    url fix

--- server-fixed/config/mongodb.js
+++ server-fixed/config/mongodb.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL + "mern-auth", {
+  await mongoose.connect(process.env.MONGODB_URL, {
     tls: true,
     tlsAllowInvalidCertificates: true,
     serverSelectionTimeoutMS: 10000,
+    dbName: "mern-auth", // ✅ alag se db name do
   });
   console.log("MongoDB Connected ✅");
 };
